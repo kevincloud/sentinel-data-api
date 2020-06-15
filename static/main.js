ffbea261d4a24cabbcc541fbe9ade02cb5844794
Kevin Cochran <kcochran@hashicorp.com>
2020-06-15 16:56:54 -0400

    update

--- static/main.js
+++ static/main.js
@@ -9,3 +9,27 @@ function deleteRequiredModule(item) {
         }
     });
 }
+
+function deleteAllowedInstance(item) {
+    $.ajax({type: "GET",
+        url: "http://localhost:8080/list/allowed-instances?remove=" + item.value,
+        success: function(result) {
+            window.location.reload();
+        },
+        error: function(result) {
+            alert(result);
+        }
+    });
+}
+
+function deleteProhibitedResource(item) {
+    $.ajax({type: "GET",
+        url: "http://localhost:8080/list/prohibited-resources?remove=" + item.value,
+        success: function(result) {
+            window.location.reload();
+        },
+        error: function(result) {
+            alert(result);
+        }
+    });
+}
