266a30f870d0c59c46f274be8691005ebc02c06a
Kevin Cochran <kcochran@hashicorp.com>
2020-06-15 18:25:40 -0400

    update

---
+++ default_data.py
@@ -0,0 +1,72 @@
+import configparser
+from azure.cosmosdb.table.tableservice import TableService
+from azure.cosmosdb.table.models import Entity
+
+config = configparser.ConfigParser()
+config.read('app.ini')
+
+identifier = config['App']['Identifier']
+account_name = identifier + "-cosmos-db"
+account_key = config['App']['AccountKey']
+table_endpoint = "https://" + identifier + "-cosmos-db.table.cosmos.azure.com:443/"
+connection_string = "DefaultEndpointsProtocol=https;AccountName=" + account_name + ";AccountKey=" + account_key + ";TableEndpoint=" + table_endpoint + ";"
+table_service = TableService(endpoint_suffix="table.cosmos.azure.com", connection_string=connection_string)
+table_name = identifier + "-cosmos-table"
+
+connection_string = "DefaultEndpointsProtocol=https;AccountName=kevincx-cosmos-db;AccountKey=u2y45eppqI8OjJbVGZxlyYBzFBVWrRdDnRNrWPLJh82F3z85Jy2RWG0nR96UUDBPVcIznqeggsUokOdqYdxquA==;TableEndpoint=https://kevincx-cosmos-db.table.cosmos.azure.com:443/;"
+table_service = TableService(endpoint_suffix="table.cosmos.azure.com", connection_string=connection_string)
+
+item = Entity()
+item.PartitionKey = "required-modules"
+item.RowKey = "custom-vnet"
+table_service.insert_entity('kevincx-cosmos-table', item)
+
+item = Entity()
+item.PartitionKey = "required-modules"
+item.RowKey = "custom-sg"
+table_service.insert_entity('kevincx-cosmos-table', item)
+
+item = Entity()
+item.PartitionKey = "required-modules"
+item.RowKey = "custom-blob"
+table_service.insert_entity('kevincx-cosmos-table', item)
+
+item = Entity()
+item.PartitionKey = "approved-instances"
+item.RowKey = "Standard_A1_v2"
+table_service.insert_entity('kevincx-cosmos-table', item)
+
+item = Entity()
+item.PartitionKey = "approved-instances"
+item.RowKey = "Standard_A2_v2"
+table_service.insert_entity('kevincx-cosmos-table', item)
+
+item = Entity()
+item.PartitionKey = "approved-instances"
+item.RowKey = "Standard_A4_v2"
+table_service.insert_entity('kevincx-cosmos-table', item)
+
+item = Entity()
+item.PartitionKey = "approved-instances"
+item.RowKey = "Standard_A8_v2"
+table_service.insert_entity('kevincx-cosmos-table', item)
+
+item = Entity()
+item.PartitionKey = "prohibited-resources"
+item.RowKey = "azurerm_resource_group"
+table_service.insert_entity('kevincx-cosmos-table', item)
+
+item = Entity()
+item.PartitionKey = "prohibited-resources"
+item.RowKey = "azurerm_virtual_network"
+table_service.insert_entity('kevincx-cosmos-table', item)
+
+item = Entity()
+item.PartitionKey = "prohibited-resources"
+item.RowKey = "azurerm_network_security_group"
+table_service.insert_entity('kevincx-cosmos-table', item)
+
+item = Entity()
+item.PartitionKey = "prohibited-resources"
+item.RowKey = "azurerm_subnet_network_security_group_association"
+table_service.insert_entity('kevincx-cosmos-table', item)
--- static/main.js
+++ static/main.js
@@ -24,9 +24,35 @@ function deleteRequiredModule(item) {
     });
 }
 
-function deleteAllowedInstance(item) {
+function addApprovedInstance(item) {
+    $.ajax({method: "GET",
+        url: "http://" + document.location.hostname + ":8080/list/approved-instances?add=" + item.value,
+    }).done(function(result) {
+        window.location.reload();
+    }).fail(function(xhr, status, error) {
+        xhr.textStatus = status;
+        xhr.errorThrown = error;
+        console.log('Error', xhr);
+        alert(xhr);
+    });
+}
+
+function deleteApprovedInstance(item) {
     $.ajax({method: "GET",
         url: "http://" + document.location.hostname + ":8080/list/approved-instances?remove=" + item.value
+    }).done(function(result) {
+        window.location.reload();
+    }).fail(function(xhr, status, error) {
+        xhr.textStatus = status;
+        xhr.errorThrown = error;
+        console.log('Error', xhr);
+        alert(xhr);
+    });
+}
+
+function addProhibitedResource(item) {
+    $.ajax({method: "GET",
+        url: "http://" + document.location.hostname + ":8080/list/prohibited-resources?add=" + item.value,
     }).done(function(result) {
         window.location.reload();
     }).fail(function(xhr, status, error) {
