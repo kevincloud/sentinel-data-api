9f1cd6866f5b28076f6095039af39a1374974206
Kevin Cochran <kcochran@hashicorp.com>
2020-06-15 16:43:27 -0400

    update

--- app.py
+++ app.py
@@ -15,14 +15,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     res = requests.get('http://localhost:8080/list/required-modules')
-    reqmods = json.loads(res)
-    # reqmods = [
-    #     "custom-vnet",
-    #     "custom-sg",
-    #     "custom-blog"
-    # ]
+    reqmods = json.loads(res.text)
 
-    return render_template("index.html", reqmods=reqmods)
+    res = requests.get('http://localhost:8080/list/approved-instances')
+    appinst = json.loads(res.text)
+
+    res = requests.get('http://localhost:8080/list/prohibited-resources')
+    probres = json.loads(res.text)
+
+    return render_template("index.html", reqmods=reqmods, appinst=appinst, probres=probres)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
